00ea55a6925346e1bf5499d0c4244eee958dd94a
danielhyduke <danielhyduke@bdb16a40-7426-4920-b3c5-7baf3776c1aa>
2011-07-25 22:32:44 +0000

    adding comments

--- pycobra/cobra/stats/stats.py
+++ pycobra/cobra/stats/stats.py
@@ -4,46 +4,65 @@ from numpy import mean, array, log10, isinf, sign, sum
 from scipy import log
 from scipy.stats import t, chi2, norm
 
-def p_adjust(p_list, correction_method='bh'):
+def p_adjust(the_p_values, correction_method='bh'):
+    """Adjusts the p-values in a list for multiple hypothesis testing.
+
+    the_p_values: a list of p-values
+
+    correction_method: String.  'bh'|'by'|'bonferroni'.  'bh' for
+    Benjamini & Hochberg, 'by' for Benjamini & Yekuieli, and
+    'bonferroni' for Bonferroni.
+
+    NOTE: 'bh' and 'by' require R and rpy2 installed
+    
+    """
     correction_method = correction_method.lower()
     if correction_method == 'bh' or correction_method == 'by':
         correction_method = correction_method.upper()
     if correction_method == 'bonferroni':
-        p_list = [min(1.0, x * float(len(p_list))) for x in p_list]
+        the_p_values = [min(1.0, x * float(len(the_p_values))) for x in the_p_values]
     else:
         #BUG: This may need to be reworked for the jython set up.
         try:
             from cobra.stats import r
         except Exception as e:
             print "Couldn't load cobra.r perhaps no rpy2 modules:%s'"%e
-        p_list = [float(x)
-                   for x in list(array(r.adjust_p(array(p_list),
+        the_p_values = [float(x)
+                   for x in list(array(r.adjust_p(array(the_p_values),
                                                   correction_method)))]
-    return p_list
+    return the_p_values
 
 def combine_p_values(the_p_values, method='z'):
-        if len(the_p_values) == 1 or sum(the_p_values) == 0:
-            combined_p_value = the_p_values[0]
-        elif method.lower() == 'z':
-            #combine p-values using weighted z-score.  To not deal with inifinite
-            #values replace 
-            new_p = []
-            for the_p in the_p_values:
-                the_p = norm.ppf(1.-the_p)
-                if isinf(the_p):
-                    if sign(the_p) < 0:
-                        the_p = -7.
-                    else:
-                        the_p = 7.
-            new_p.append(the_p)
-            combined_p_value = norm.sf(sum(new_p) / len(new_p)**0.5)
-        elif method.lower() == 'fisher':
-            combined_p_value = 1-chi2.cdf(-2*sum(map(log,
-                                                        the_p_values)),
-                                             2*len(the_p_values))
+    """Combines p-values from repeat measurements into a single
+    p-value.
+
+    the_p_values: a list of p-values.
+
+    method: String. 'z'|'fisher'.  'z' for using the weighted z-score.
+    'fisher' for using fisher's combined probability test.
+    """
+    if len(the_p_values) == 1 or sum(the_p_values) == 0:
+        combined_p_value = the_p_values[0]
+    elif method.lower() == 'z':
+        #combine p-values using weighted z-score.  To not deal with inifinite
+        #values replace 
+        new_p = []
+        for the_p in the_p_values:
+            the_p = norm.ppf(1.-the_p)
+            if isinf(the_p):
+                if sign(the_p) < 0:
+                    the_p = -7.
+                else:
+                    the_p = 7.
+        new_p.append(the_p)
+        combined_p_value = norm.sf(sum(new_p) / len(new_p)**0.5)
+    elif method.lower() == 'fisher':
+        combined_p_value = 1-chi2.cdf(-2*sum(map(log,
+                                                    the_p_values)),
+                                         2*len(the_p_values))
 
 
-        return combined_p_value
+    return combined_p_value
 
 def error_weighted(the_means, the_stds):
     """Calculate the error-weighted mean and standard deviation.
